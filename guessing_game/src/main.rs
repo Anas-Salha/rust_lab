b1a8566bdc3b4a89056b52661fec7ed7b674cd12
Anas Salha <anassalha96@gmail.com>
2023-03-25 02:30:10 +0100

    Completed guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,45 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     let game_name = "Guessing Game";
-    let game_prompt = "Place your guess: ";
-    println!("Welcome to {}!\n{}", game_name, game_prompt);
+    let game_prompt = "Place your guess (1-100): ";
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let max_attempts = 5;
 
-    let mut user_guess = String::new();
+    println!("Welcome to {game_name}!");
+    println!("You have {max_attempts} lives. Guess a randomly generated number (1-100)");
+
     
-    io::stdin()
-        .read_line(&mut user_guess)
-        .expect("Failed to read line");
+    for attempt in 0..max_attempts{
+        let mut user_guess = String::new();
+        println!("Attempt {} - {game_prompt}", attempt + 1);
 
+        io::stdin()
+            .read_line(&mut user_guess)
+            .expect("Failed to read line");
+        
+        if user_guess == "q\n"{
+            println!("Quitter :).");
+            return;
+        }
+
+        let user_guess: u32 = match user_guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please select a number. Way to waste a life, buddy.");
+                continue;
+            }
+        };
+        
+        match user_guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small! Git gud."),
+            Ordering::Greater => println!("Too big! Git gud."),
+            Ordering::Equal => {
+                println!("Bingo! You guessed the correct number!");
+                break;
+            }
+        }
+    }
 }
