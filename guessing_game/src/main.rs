71e7d0645e99898485a75ce56d18eeea2e783730
Anas Salha <anassalha96@gmail.com>
2023-04-02 01:06:56 +0200

    Increased number of attempts to make guessing_game always winnable

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,21 +1,20 @@
 // Reference: The Rust Programming Language (online book)
 // Ch2 - https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     let game_name = "Guessing Game";
     let game_prompt = "Place your guess (1-100): ";
     let secret_number = rand::thread_rng().gen_range(1..=100);
-    let max_attempts = 5;
+    let max_attempts = 7;
 
     println!("Welcome to {game_name}!");
     println!("You have {max_attempts} lives. Guess a randomly generated number (1-100)");
 
-
-    for attempt in 0..max_attempts{
+    for attempt in 0..max_attempts {
         let mut user_guess = String::new();
         println!("Attempt {} - {game_prompt}", attempt + 1);
 
@@ -23,7 +22,7 @@ fn main() {
             .read_line(&mut user_guess)
             .expect("Failed to read line");
 
-        if user_guess == "q\n"{
+        if user_guess == "q\n" {
             println!("Quitter :).");
             return;
         }
@@ -41,8 +40,9 @@ fn main() {
             Ordering::Greater => println!("Too big! Git gud."),
             Ordering::Equal => {
                 println!("Bingo! You guessed the correct number!");
-                break;
+                return;
             }
         }
     }
+    println!("The number was {secret_number}. Better luck next time!");
 }
